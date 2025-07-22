0844ef790beaa5533019af4473a10491aeb68da4
namr03 <nam.rzedziak@wp.pl>
2025-07-22 19:26:57 +0200

    small fixes with ports

--- booksyv2/backend/booksyv2/settings.py
+++ booksyv2/backend/booksyv2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&+dcod&zp0o5an&1(z_2gi3_(*n*&nn=4ouck_#g=k9fu#cyn-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend','frontend']
 
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://127.0.0.1']
